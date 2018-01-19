5670f74636236dc8208de84ba9ae877a18f2a15d
KanagarajPonnusamy <kanagupbk@gmail.com>
2018-01-19 10:09:44 +0530

    Added Calculations file

---
+++ RithuAnu/Calculations.swift
@@ -0,0 +1,21 @@
+//
+//  Calculations.swift
+//  RithuAnu
+//
+//  Created by Kanagaraj on 19/01/18.
+//  Copyright © 2018 Trellisys. All rights reserved.
+//
+
+import UIKit
+
+class Calculations: NSObject {
+
+    
+    public func addNumbers(num1 : Int, num2 : Int) -> Int {
+        return num1 + num2
+    }
+    
+    public func multiplyNumbers(num1 : Int, num2 : Int) -> Int {
+        return num1 * num2
+    }
+}
